295136ec120cead6e09e42e72b01bcbb102f7823
Alexander Neitzel <alexander.neitzel@garmingo.com>
2025-02-11 18:37:42 +0100

    Add initial implementation for Bus Driver Pro - Qasar Vehicle Keys integration

---
+++ BusDriverPro/bus-driver-pro-qs-vehiclekeys/client.lua
@@ -0,0 +1,7 @@
+AddEventHandler("BusDriverPro:CarSpawned", function(handle, plate)
+    local model = GetDisplayNameFromVehicleModel(GetEntityModel(handle))
+
+    SetVehicleDoorsLocked(handle, 1) 
+
+    exports['qs-vehiclekeys']:GiveKeys(plate, model, true)
+end)
---
+++ BusDriverPro/bus-driver-pro-qs-vehiclekeys/fxmanifest.lua
@@ -0,0 +1,16 @@
+fx_version 'cerulean'
+games { 'gta5' }
+
+author 'Garmingo <support@garmingo.com>'
+description 'Bus Driver Pro - Qasar Vehicle Keys'
+version '1.0.0'
+
+client_scripts {
+    'client.lua',
+}
+
+dependencies {
+    -- Change these if you renamed the resources
+    'BusDriverPro',
+    'qs-vehiclekeys'
+}
